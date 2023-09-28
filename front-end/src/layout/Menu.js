b86c8cc4907fb67bbf805b81cdafa89623e5f579
Stefanie Engstrom <38473399+s3ngstrom@users.noreply.github.com>
2023-09-27 23:51:15 -0500

    Updates to mobile responsiveness

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -8,25 +8,19 @@ import { Link } from "react-router-dom";
  */
 function Menu() {
   return (
-    <nav className="navbar navbar-dark align-items-start p-0">
-      {/* Container for navigation items */}
+<nav className="navbar navbar-dark align-items-start p-0">
       <div className="container-fluid d-flex flex-column p-0">
-        {/* Brand link to the home page */}
         <Link
           className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0"
           to="/"
         >
-          <div className="sidebar-brand-text mx-3 font-weight-bold">
-            {/* Title for the brand */}
+          <div className="sidebar-brand-text mx-3">
             <span>Periodic Tables</span>
           </div>
         </Link>
-        {/* Divider between the brand and navigation items */}
         <hr className="sidebar-divider my-0" />
-        {/* List of navigation items */}
         <ul className="nav navbar-nav text-light" id="accordionSidebar">
           <li className="nav-item">
-            {/* Link to the dashboard */}
             <Link className="nav-link" to="/dashboard">
               <span className="oi oi-dashboard" />
               &nbsp;Dashboard
@@ -35,33 +29,22 @@ function Menu() {
           <li className="nav-item">
             {/* Link to the search page */}
             <Link className="nav-link" to="/search">
-              <span className="oi oi-magnifying-glass" />
-              &nbsp;Search
+              Search
             </Link>
           </li>
           <li className="nav-item">
             {/* Link to the new reservation page */}
             <Link className="nav-link" to="/reservations/new">
-              <span className="oi oi-plus" />
-              &nbsp;New Reservation
+              New Reservation
             </Link>
           </li>
           <li className="nav-item">
             {/* Link to the new table page */}
             <Link className="nav-link" to="/tables/new">
-              <span className="oi oi-layers" />
-              &nbsp;New Table
+              New Table
             </Link>
           </li>
         </ul>
-        {/* Hidden toggle button for small screen devices */}
-        <div className="text-center d-none d-md-inline">
-          <button
-            className="btn rounded-circle border-0"
-            id="sidebarToggle"
-            type="button"
-          />
-        </div>
       </div>
     </nav>
   );
